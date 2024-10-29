f4a5ad332b9405954c6d93ed579785c1671bd323
서샛별 <seosaesbyeol@seosaesbyeol-ui-noteubug.local>
2024-10-29 13:18:51 +0900

    update: buttom 공용컴포넌트 구현

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,19 @@ interface ButtonProps {
   type?: "button" | "submit";
   onClick?: () => void;
   children: React.ReactNode;
+  isActive?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children, type }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, children, type = "button", isActive = true }) => {
   return (
-    <button type={type} onClick={onClick} className="">
+    <button
+      type={type}
+      onClick={onClick}
+      className={`flex w-[312px] px-[165px] py-4 justify-center items-center rounded-md 
+        transition-all duration-300 truncate
+        ${isActive ? "bg-[#00C853] opacity-100" : "bg-[#2E2E2E] opacity-60 cursor-not-allowed"}`}
+      disabled={!isActive}
+    >
       {children}
     </button>
   );
