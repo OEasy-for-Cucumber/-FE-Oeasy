752366a0f30fa39611bb8b5446176f5e4c175ffe
parksumi <parksumi0127@gmail.com>
2024-10-29 15:27:37 +0900

    style: Input컴포넌트 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,17 @@
 interface InputProps {
   type?: "text" | "password" | "number" | "email";
+  value?: string;
   placeholder?: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder }) => {
+const Input: React.FC<InputProps> = ({ type, value, onChange, placeholder }) => {
   return (
     <input
       type={type}
-      className="w-[312px] h-[48px] placeholder-grayoe-400 bg-grayoe-950 outline-none"
+      value={value}
+      onChange={onChange}
+      className="w-[312px] h-[48px] placeholder-grayoe-400 bg-grayoe-950 outline-none border-b-[1.5px] border-grayoe-700"
       placeholder={placeholder}
     />
   );
