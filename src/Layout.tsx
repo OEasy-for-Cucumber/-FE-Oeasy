65cc369d60d23cdbd3806daa2713d5aecf0e0df4
parksumi <parksumi0127@gmail.com>
2024-11-04 16:54:47 +0900

    add: Recipe컴포넌트 생성

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC<layoutProps> = ({ children }) => {
   const location = useLocation();
 
   return (
-    <div className={`${location.pathname === "/" ? "bg-greenoe-950" : "bg-grayoe-950"} h-screen`}>
+    <div className={`${location.pathname === "/" ? "bg-greenoe-950" : "bg-grayoe-950 h-screen"}`}>
       <div className={`${location.pathname === "/" ? "" : "px-6"} mx-auto min-w-[360px] max-w-[500px] xl:max-w-none `}>
         {children}
       </div>
