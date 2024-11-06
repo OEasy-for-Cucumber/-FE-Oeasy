4242453a3e78334d54c02e23240fa4901ec61695
서샛별 <seosaesbyeol@seosaesbyeol-ui-noteubug.local>
2024-11-06 21:03:38 +0900

    del: 레이아웃 붎필요한 공백 제거

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -10,7 +10,6 @@ const Layout: React.FC<layoutProps> = ({ children }) => {
 
   return (
     <div className={`${location.pathname === "/" ? "bg-greenoe-950" : "bg-grayoe-950 h-screen"}`}>
-
       <div className={`${noneLayout ? "" : "px-6"} mx-auto min-w-[360px] max-w-[500px] xl:max-w-none `}>{children}</div>
     </div>
   );
