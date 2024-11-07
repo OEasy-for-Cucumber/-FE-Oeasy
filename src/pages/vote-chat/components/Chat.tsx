c346ef452c304b75b64cc11e29e404ab4e31dde7
lyuna29 <hyunah0418@gmail.com>
2024-11-07 17:04:01 +0900

    style: votechat 반응형 css

--- src/pages/vote-chat/components/Chat.tsx
+++ src/pages/vote-chat/components/Chat.tsx
@@ -42,42 +42,48 @@ function Chat() {
   };
 
   return (
-    <div className="relative flex flex-col h-auto pt-[16px] xl:w-[488px] xl:h-[686px]">
-      <div className="flex flex-col px-[16px]  overflow-y-auto gap-4">
-        {messages.map((chat) => (
-          <div key={chat.id} className="flex items-start">
-            <img src={chat.profileImg} alt="Profile" className="w-10 h-10 rounded-full mr-[8px] border border-white" />
-            <div className="flex flex-col gap-[4px]">
-              <p className="font-semibold">{chat.username}</p>
-              <div className="relative bg-[url('/img/speechBubble.png')] bg-no-repeat bg-cover rounded-r-lg  max-w-[280px] min-w-[20px] w-auto">
-                <p className="text-white font-b2-regular break-words px-[12px] py-[8px] whitespace-pre-wrap">
-                  {chat.content}
-                </p>
+    <div className="flex justify-center items-center">
+      <div className="relative pt-[16px] xl:w-[488px] xl:h-[686px]">
+        <div className="flex flex-col px-[16px] xl:h-[634px] overflow-y-auto gap-4">
+          {messages.map((chat) => (
+            <div key={chat.id} className="flex items-start">
+              <img
+                src={chat.profileImg}
+                alt="Profile"
+                className="w-10 h-10 rounded-full mr-[8px] border border-white"
+              />
+              <div className="flex flex-col gap-[4px]">
+                <p className="font-semibold">{chat.username}</p>
+                <div className="relative bg-[url('/img/speechBubble.png')] bg-no-repeat bg-cover rounded-r-lg  max-w-[280px] min-w-[20px] w-auto">
+                  <p className="text-white font-b2-regular break-words px-[12px] py-[8px] whitespace-pre-wrap">
+                    {chat.content}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-        <div ref={messageEndRef} />
-      </div>
+          ))}
+          <div ref={messageEndRef} />
+        </div>
 
-      <div className="sticky w-full h-[52px] flex justify-center items-center px-[16px] py-[8px] bg-gray-800 bottom-0 left-0 right-0">
-        <div className="relative w-[340px]">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={handleKeyDown}
-            onCompositionStart={() => setComposing(true)}
-            onCompositionEnd={() => setComposing(false)}
-            placeholder="메시지를 입력해주세요"
-            className="w-full h-[36px] p-2 pl-[24px] pr-[54px] rounded-[999px] focus:outline-none bg-grayoe-400 placeholder-grayoe-200"
-          />
-          <button
-            onClick={handleSendMessage}
-            className="absolute right-1 top-1 bottom-1 w-[44px] h-[28px] bg-greenoe-600 text-white rounded-[999px] flex items-center justify-center"
-          >
-            <img src={sendIcon} alt="Send" className="w-[20px] h-[20px]" />
-          </button>
+        <div className="sticky w-full h-[52px] flex justify-center items-center px-[16px] py-[8px] bg-gray-800 bottom-0 left-0 right-0">
+          <div className="relative w-[340px] xl:w-[456px]">
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+              onCompositionStart={() => setComposing(true)}
+              onCompositionEnd={() => setComposing(false)}
+              placeholder="메시지를 입력해주세요"
+              className="w-full h-[36px] p-2 pl-[24px] pr-[54px] rounded-[999px] focus:outline-none bg-grayoe-400 placeholder-grayoe-200"
+            />
+            <button
+              onClick={handleSendMessage}
+              className="absolute right-1 top-1 bottom-1 w-[44px] h-[28px] bg-greenoe-600 text-white rounded-[999px] flex items-center justify-center"
+            >
+              <img src={sendIcon} alt="Send" className="w-[20px] h-[20px]" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/vote-chat/index.tsx
+++ src/pages/vote-chat/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Chat from "./components/Chat";
 import Vote from "./components/Vote";
-import useWebActive from "./hooks/WebActive";
+import useWebActive from "./hooks/useWebActive";
 
 function Votechat() {
   const [active, setActive] = useState<"vote" | "chat">("vote");
@@ -23,7 +23,7 @@ function Votechat() {
     <div className="flex flex-col xl:flex-row xl:justify-center xl:items-center h-[calc(100vh-56px)] xl:h-[calc(100vh-80px)] overflow-hidden transition-all duration-500 ease-in-out">
       <div
         className={`transition-all duration-500 ease-in-out overflow-hidden ${
-          active === "vote" ? "h-[40%] " : "h-[20%] "
+          active === "vote" ? "h-[40%] xl:h-auto" : "h-[20%] xl:h-auto"
         }`}
         onClick={handleVoteClick}
       >
@@ -32,7 +32,7 @@ function Votechat() {
       <div className="border-grayoe-900 border-[6px] w-full xl:hidden" />
       <div
         className={`flex transition-all duration-500 ease-in-out overflow-auto ${
-          active === "chat" ? "h-[80%] " : "h-[60%] "
+          active === "chat" ? "h-[80%] xl:h-full" : "h-[60%] xl:h-full"
         }`}
         onClick={handleChatClick}
       >
