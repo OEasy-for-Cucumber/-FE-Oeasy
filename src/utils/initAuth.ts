f673521f95b254d3f0d6141e49041fb899130142
parksumi <parksumi0127@gmail.com>
2024-11-08 16:16:51 +0900

    style: 랜딩페이지 타이틀 반응형 작업 중

--- src/utils/initAuth.ts
+++ src/utils/initAuth.ts
@@ -2,23 +2,31 @@ import { useEffect } from "react";
 import { useUserStore } from "../zustand/authStore";
 import instance from "../api/axios";
 
-async function useUserInitialize () {
-    const { setUser, setIsLoggedIn, setIsInitialized } = useUserStore.getState();
+function useUserInitialize() {
+  const { setUser, setIsLoggedIn, setIsInitialized } = useUserStore.getState();
 
-    useEffect(()=>{
-        const initUser = async () => {
-            const { data } = await instance.get("/member/profile");
-            const token = localStorage.getItem("accessToken");
-            if(data) {
-                setUser(data);
-            }
-            setIsInitialized(true);
-            
-            if(token) setIsLoggedIn(true);
+  useEffect(() => {
+    const initUser = async () => {
+      try {
+        const { data } = await instance.get("/member/profile");
+        const token = localStorage.getItem("accessToken");
+
+        if (data) {
+          setUser(data);
         }
-        initUser()
-    },[setUser, setIsInitialized])
 
+        setIsInitialized(true);
+
+        if (token) {
+          setIsLoggedIn(true);
+        }
+      } catch (error) {
+        console.error("사용자 초기화 오류:", error);
+      }
+    };
+
+    initUser();
+  }, [setUser, setIsLoggedIn, setIsInitialized]);
 }
 
-export default useUserInitialize
+export default useUserInitialize;
