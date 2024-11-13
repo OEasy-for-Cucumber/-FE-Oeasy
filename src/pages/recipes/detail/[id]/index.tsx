1a089a05658a7455e034a1c520647a20be453836
parksumi <parksumi0127@gmail.com>
2024-11-13 20:32:38 +0900

    style: 레시피 detail 완성

--- src/pages/recipes/detail/[id]/index.tsx
+++ src/pages/recipes/detail/[id]/index.tsx
@@ -1,36 +1,48 @@
 import { useParams } from "react-router-dom";
-import useRecipesData from "../../../../hooks/useRecipesData";
 import { useEffect, useState } from "react";
-import { Recipe } from "../../../../types/recipes";
+import instance from "../../../../api/axios";
+import { selectRecipe } from "../../../../types/recipes";
 
 function RecipeDetail() {
   const { id } = useParams();
-  const { data: recipes } = useRecipesData();
-  const [storedRecipe, setStoredRecipe] = useState<Recipe>();
+  const [selectRecipe, setSelectRecipe] = useState<selectRecipe>();
 
-  const recipe = recipes?.pages.flatMap((page) => page.list).find((recipe) => String(recipe.id) == id);
-
-  useEffect(() => {
-    if (recipe) {
-      localStorage.setItem("recipeDetail", JSON.stringify(recipe));
-    }
-  }, [recipe]);
+  const body2 = "font-b2-regular text-grayoe-100";
 
   useEffect(() => {
-    const savedRecipe = localStorage.getItem("recipeDetail");
-    if (savedRecipe) {
-      setStoredRecipe(JSON.parse(savedRecipe));
-    }
-  }, []);
+    const selectRecipeData = async () => {
+      try {
+        const res = await instance.get(`/api/recipe?id=${id}`);
+        setSelectRecipe(res?.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    selectRecipeData();
+  }, [id]);
 
   return (
     <section>
-      {storedRecipe ? (
+      {selectRecipe && (
         <>
-          <img src={storedRecipe.imgUrl} alt={storedRecipe.title} />
+          <img src={selectRecipe.recipeImg} alt={selectRecipe.title} />
+          <div className="mx-6 my-[34px]">
+            <h5 className="font-h5 mb-2">{selectRecipe.title}</h5>
+            <p className="font-b1-regular">{selectRecipe.tip}</p>
+          </div>
+
+          <div className="h-2 bg-grayoe-900 mb-6"></div>
+
+          <div className="mx-6 border-b border-grayoe-900 pb-4 mb-4">
+            <p className={body2}>재료</p>
+            <p>{selectRecipe.ingredients}</p>
+          </div>
+
+          <div className="mx-6 mb-6">
+            <p className={body2}>조리과정</p>
+            <ul>{selectRecipe.manualList?.map((list) => <li key={list.order}>{list.content}</li>)}</ul>
+          </div>
         </>
-      ) : (
-        <p>Loading...</p>
       )}
     </section>
   );
