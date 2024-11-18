7d0bb007a3ec427f4f0fe175d4b24eee3c11146a
서샛별 <seosaesbyeol@seosaesbyeol-ui-noteubug.local>
2024-11-18 17:28:19 +0900

    fix: https 임시 주석처리

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
+// import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,10 +9,10 @@ export default defineConfig({
     global: "window"
   },
   server: {
-    https: {
-      key: fs.readFileSync("./localhost+2-key.pem"),
-      cert: fs.readFileSync("./localhost+2.pem")
-    },
+    // https: {
+    //   key: fs.readFileSync("./localhost+2-key.pem"),
+    //   cert: fs.readFileSync("./localhost+2.pem")
+    // },
     host: "localhost",
     port: 3000
   }
