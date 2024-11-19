9d50d0be10d3328e03a85d9c560d0d5cc7e0d7d0
lyuna29 <hyunah0418@gmail.com>
2024-11-19 15:27:43 +0900

    fix:vote 이미지 수정

--- src/pages/community/upload/[id]/index.tsx
+++ src/pages/community/upload/[id]/index.tsx
@@ -55,7 +55,7 @@ function Upload() {
 
   return (
     <>
-      <div>
+      <div className="xl:w-[767px] mx-auto">
         <div className="flex justify-between py-4 px-6">
           <p className="font-h5">글쓰기</p>
           <button className="w-14 h-8 font-c2 rounded-[4px] bg-grayoe-400 px-3 py-2" onClick={handleSubmit}>
@@ -63,7 +63,7 @@ function Upload() {
           </button>
         </div>
         <div className="border-grayoe-900 border-4 w-full xl:hidden" />
-        <div className="flex flex-col justify-center items-start font-b2-regular">
+        <div className="flex flex-col justify-center items-start font-b2-regular xl:font-b1-regular">
           <input
             ref={titleRef}
             className="w-full h-auto py-4 px-6 bg-grayoe-950 outline-none"
