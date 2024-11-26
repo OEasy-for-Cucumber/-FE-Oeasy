82d10c32a5d8b243c087ca7f8d6e657f3817cd53
lyuna29 <hyunah0418@gmail.com>
2024-11-26 18:28:25 +0900

    fix:oeIndex

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,8 +4,9 @@ import OeGraph from "./components/OeGraph";
 import OeTip from "./components/OeTip";
 import AiOe from "./components/AiOe";
 import Recipe from "./components/recipe/Recipe";
-import OeIndex from "./components/OeIndex";
+
 import { useEffect, useRef, useState } from "react";
+import OeIndex from "./components/OEIndex";
 
 function Home() {
   const outerDivRef = useRef<HTMLDivElement | null>(null);
