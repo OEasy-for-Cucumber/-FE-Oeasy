98b227d1a790602807e0f69b5859da9c58c3c967
서샛별 <seosaesbyeol@seosaesbyeol-ui-noteubug.local>
2024-11-30 23:33:10 +0900

    update: 마이페이지 내가 작성한 게시물 데이터 불러오기 성공

--- src/pages/mypage/components/MyLiked.tsx
+++ src/pages/mypage/components/MyLiked.tsx
@@ -1,8 +1,22 @@
 
 function MyLiked() {
   return (
-    <div className="w-full my-5">
-      <div className="text-center">좋아요 한 게시글이 없습니다.</div>
+    <div className="w-full my-5 px-6">
+      <div className="text-center">좋아요한 게시글이 없습니다.</div>
+      
+      <div className="w-full border-b py-4">
+        <div className="flex flex-col">
+          <p>24.08.23</p>
+          <p>오이가 세상을 구한다</p>
+
+          <div className="flex">
+            <p>좋아요수</p>
+            <p>조회수</p>
+            <p>댓글 수</p>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/mypage/components/MyPost.tsx
+++ src/pages/mypage/components/MyPost.tsx
@@ -1,7 +1,64 @@
+import { useEffect, useState } from "react";
+import instance from "../../../api/axios";
+import { useUserStore } from "../../../zustand/authStore";
+import { useSearchParams } from "react-router-dom";
+import { Contents } from "../../../types/myContentsTypes";
+
 function MyPost() {
+  const [myPosts, setMyPosts] = useState<Contents[]>();
+  const [searchParams, _setSearchParams] = useSearchParams();
+
+  const user = useUserStore((state) => state.user);
+  const currentPage = parseInt(searchParams.get("page") || "1");
+
+  // 날짜 포맷팅 함수
+  const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}/${month}/${day}`;
+  };
+
+  useEffect(() => {
+    const getMyPostsData = async () => {
+      const data = await instance.get("/api/community/my-Cmn", {
+        params: {
+          page: currentPage - 1,
+          size: 10,
+          memberId: user?.memberPk,
+        },
+      });
+      // 데이터 가져온 후 변환
+      const formattedPosts = data.data.contents.map((post: Contents) => ({
+        ...post,
+        createTime: formatDate(post.createTime), // 날짜 포맷 변환
+      }));
+      setMyPosts(formattedPosts);
+    };
+    getMyPostsData();
+  }, [currentPage, user]);
+
   return (
-    <div className="w-full my-5">
-      <div className="text-center">작성한 게시글이 없습니다.</div>
+    <div className="w-full my-5 px-6">
+      {!myPosts ? (
+        <div className="text-center">작성한 게시글이 없습니다.</div>
+      ) : (
+        myPosts.map((post) => (
+          <div className="w-full border-b py-4" key={post.boardPk}>
+            <div className="flex flex-col">
+              <p>{post.createTime}</p>
+              <p>{post.title}</p>
+
+              <div className="flex space-x-2">
+                <p>{post.likeCnt > 0 ? post.likeCnt : 0}</p>
+                <p>{post.viewCnt > 0 ? post.viewCnt : 0}</p>
+                <p>{post.comentCnt > 0 ? post.comentCnt : 0}</p>
+              </div>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 }
