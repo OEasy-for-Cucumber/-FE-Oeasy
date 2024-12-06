0f6c2d9fcc7c7c6a0d8c6edcd380559f4fadfbd0
parksumi <parksumi0127@gmail.com>
2024-12-06 10:42:35 +0900

    style: oeTip 폰트 이미지 사이즈 수정

--- src/pages/home/components/OeTip.tsx
+++ src/pages/home/components/OeTip.tsx
@@ -37,20 +37,20 @@ const OeTip: FC<scrollRefProps> = ({ scrollRef }) => {
   return (
     <section className="flex flex-col items-center justify-center h-[calc(100vh-56px)] xl:h-[calc(100vh-80px)] px-[35px] xl:p-0">
       <div className="flex flex-col gap-2 w-[281px] xl:w-[500px] mx-auto">
-        <div className="font-h6 xl:font-h3 text-center">오이에 대해 얼마나 알고 계신가요?</div>
-        <div className="font-h3 xl:font-h1 text-center">우리 오이는</div>
+        <div className="font-h6 xl:font-h4 text-center">오이에 대해 얼마나 알고 계신가요?</div>
+        <div className="font-h3 xl:font-h2 text-center">우리 오이는</div>
       </div>
 
       <div
         className={`${animation.animationOne ? "animate-fade-in-up" : "opacity-0"} w-[281px] transition-opacity xl:w-full mx-auto`}
       >
-        <img src={quotes} alt="큰따옴표" className="w-[39px] xl:w-[82px] h-[29px] xl:h-[60px] my-10 mx-auto" />
+        <img src={quotes} alt="큰따옴표" className="w-[39px] xl:w-[60px] h-[29px] xl:h-[44px] my-10 mx-auto" />
       </div>
 
       {tipList && (
         <div className={`${animation.animationTwo ? "animate-fade-in-up" : "opacity-0"} transition-opacity`}>
           <img src={tipList.mobileImg} className="xl:hidden" />
-          <img src={tipList.webImg} className="hidden xl:block h-[440px]" />
+          <img src={tipList.webImg} className="hidden xl:block h-[422px]" />
         </div>
       )}
     </section>
