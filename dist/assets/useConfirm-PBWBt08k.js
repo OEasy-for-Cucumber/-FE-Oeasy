bd0598d7aed65b77194862c4488b9eb868ca7cb0
서샛별 <seosaesbyeol@seosaesbyeol-ui-noteubug.local>
2024-12-18 22:15:33 +0900

    pull: 현아님 코드

---
+++ dist/assets/useConfirm-PBWBt08k.js
@@ -0,0 +1,1 @@
+import{r as o,C as t}from"./index-JxLw885_.js";const n=()=>{const r=o.useContext(t);if(!r)throw new Error("useConfirm must be used within a ConfirmProvider");return r};export{n as u};
