5f77cc25aaf818d521a4e57b926bf3c2952bfa22
lyuna29 <hyunah0418@gmail.com>
2025-01-03 15:38:58 +0900

    fix:chat padding

--- src/pages/vote-chat/components/Chat.tsx
+++ src/pages/vote-chat/components/Chat.tsx
@@ -95,7 +95,7 @@ function Chat({ chattingList }: ChatProps) {
     const scrollToBottom = () => {
       if (messageEndRef.current) {
         messageEndRef.current.scrollIntoView({
-          block: "end" // 하단 정렬
+          block: "end"
         });
       }
     };
@@ -137,37 +137,39 @@ function Chat({ chattingList }: ChatProps) {
   };
 
   return (
-    <div className="flex flex-col justify-between w-full xl:w-[488px] xl:h-[686px] xl:my-auto xl:bg-grayoe-900 xl:rounded-2xl ">
-      <div className="relative pt-4 mx-4 xl:mx-0">
-        <div className="flex flex-col xl:px-4 xl:h-[634px] overflow-y-auto scrollbar-hidden gap-4  ">
-          {messages.map((msg, index) =>
-            msg.nickname === user?.nickname ? (
-              <div key={`${msg.id}-${index}`} className="flex justify-end items-start">
-                <div className="flex flex-col items-end gap-1 max-w-[280px] min-w-[20px]">
-                  <p className="font-semibold">{msg.nickname}</p>
-                  <div className="bg-grayoe-600 rounded-l-xl rounded-br-xl px-3 py-2 text-white break-words whitespace-pre-wrap">
-                    {msg.content}
+    <div className="flex flex-col justify-between w-full xl:w-[488px] xl:h-[686px] xl:my-auto xl:bg-grayoe-900 xl:rounded-2xl">
+      <div className="relative flex-1 overflow-hidden">
+        <div className="flex flex-col h-full overflow-y-auto scrollbar-hidden">
+          <div className="flex-1 pt-4 px-4  space-y-4">
+            {messages.map((msg, index) =>
+              msg.nickname === user?.nickname ? (
+                <div key={`${msg.id}-${index}`} className="flex justify-end items-start">
+                  <div className="flex flex-col items-end gap-1 max-w-[280px] min-w-[20px]">
+                    <p className="font-semibold">{msg.nickname}</p>
+                    <div className="bg-grayoe-600 rounded-l-xl rounded-br-xl px-3 py-2 text-white break-words whitespace-pre-wrap">
+                      {msg.content}
+                    </div>
+                  </div>
+                  <img src={msg.profileImg} alt="Profile" className="w-10 h-10 rounded-full ml-2" />
+                </div>
+              ) : (
+                <div key={`${msg.id}-${index}`} className="flex justify-start items-start">
+                  <img src={msg.profileImg} alt="Profile" className="w-10 h-10 rounded-full mr-2" />
+                  <div className="flex flex-col gap-1 max-w-[280px] min-w-[20px]">
+                    <p className="font-semibold">{msg.nickname}</p>
+                    <div className="bg-grayoe-600 rounded-r-xl rounded-bl-xl px-3 py-2 text-white break-words whitespace-pre-wrap">
+                      {msg.content}
+                    </div>
                   </div>
                 </div>
-                <img src={msg.profileImg} alt="Profile" className="w-10 h-10 rounded-full ml-2" />
-              </div>
-            ) : (
-              <div key={`${msg.id}-${index}`} className="flex justify-start items-start">
-                <img src={msg.profileImg} alt="Profile" className="w-10 h-10 rounded-full mr-2 " />
-                <div className="flex flex-col gap-1 max-w-[280px] min-w-[20px]">
-                  <p className="font-semibold">{msg.nickname}</p>
-                  <div className="bg-grayoe-600 rounded-r-xl rounded-bl-xl px-3 py-2 text-white break-words whitespace-pre-wrap">
-                    {msg.content}
-                  </div>
-                </div>
-              </div>
-            )
-          )}
-          <div ref={messageEndRef} />
+              )
+            )}
+            <div ref={messageEndRef} />
+          </div>
         </div>
       </div>
-      <div className="sticky w-full h-[52px] flex justify-center items-center px-4 py-2 bg-grayoe-800 bottom-0 xl:rounded-b-2xl">
-        <div className="relative w-full xl:w-full">
+      <div className="sticky bottom-0 w-full bg-grayoe-800 px-4 py-2 xl:rounded-b-2xl">
+        <div className="relative w-full">
           <input
             type="text"
             value={message}
