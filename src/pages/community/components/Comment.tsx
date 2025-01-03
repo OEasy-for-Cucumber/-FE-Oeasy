9e3c570f8f02f8b8e79f361c5b6af3e600eb9a1e
lyuna29 <hyunah0418@gmail.com>
2025-01-03 15:47:47 +0900

    community 댓글 입력창

--- src/pages/community/components/Comment.tsx
+++ src/pages/community/components/Comment.tsx
@@ -7,6 +7,7 @@ import useConfirm from "@/hooks/useConfirm";
 import useAlert from "@/hooks/useAlert";
 import instance from "@/api/axios";
 import Pagination from "./Pagination";
+import defaultProfileImg from "@/assets/img/defaultProfile.webp";
 
 interface CmnProps {
   communityId: number;
@@ -25,14 +26,14 @@ interface Comment {
 function Comment({ communityId, setTotalComments }: CmnProps) {
   const [showMenu, setShowMenu] = useState<boolean>(false);
   const [activeCommentPk, setActiveCommentPk] = useState<number | null>(null);
-  const [editingComment, setEditingComment] = useState<number | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
   const user = useUserStore((state) => state.user);
   const [comments, setComments] = useState<Comment[]>([]);
   const [totalPages, setTotalPages] = useState(0);
   const [commentContent, setCommentContent] = useState<string>("");
   const [isSending, setIsSending] = useState(false);
-  const [editContent, setEditContent] = useState<string>("");
+  const [editingMode, setEditingMode] = useState(false);
+  const [editingCommentPk, setEditingCommentPk] = useState<number | null>(null);
   const { showConfirm } = useConfirm();
   const { showAlert } = useAlert();
 
@@ -82,12 +83,18 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
 
   const handleSendComment = async () => {
     if (isSending) return;
-    if (!commentContent.trim) {
+    if (!commentContent.trim()) {
       showAlert({
         message: "댓글을 입력해주세요"
       });
       return;
     }
+
+    if (editingMode) {
+      handleEditSubmit();
+      return;
+    }
+
     setIsSending(true);
     const requestComment = {
       communityId,
@@ -108,21 +115,25 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
   };
 
   const handleEdit = (commentPk: number, content: string) => {
-    setEditingComment(commentPk);
-    setEditContent(content);
+    setEditingMode(true);
+    setEditingCommentPk(commentPk);
+    setCommentContent(content);
     setShowMenu(false);
   };
 
-  const handleEditSubmit = async (commentPk: number) => {
-    const prevComment = comments.find((comment) => comment.commentPk === commentPk);
+  const handleEditSubmit = async () => {
+    if (!editingCommentPk) return;
 
-    if (prevComment?.content === editContent.trim()) {
+    const prevComment = comments.find((comment) => comment.commentPk === editingCommentPk);
+
+    if (prevComment?.content === commentContent.trim()) {
       showAlert({
         message: "변경된 내용이 없습니다"
       });
       return;
     }
-    if (!editContent.trim()) {
+
+    if (!commentContent.trim()) {
       showAlert({
         message: "수정할 내용을 입력해주세요."
       });
@@ -131,17 +142,18 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
 
     const requestEdit = {
       memberId: user?.memberPk,
-      commentId: commentPk,
-      content: editContent
+      commentId: editingCommentPk,
+      content: commentContent
     };
 
     try {
       await instance.patch("/api/community/comment", requestEdit);
-
       showAlert({
         message: "댓글이 수정되었습니다."
       });
-      setEditingComment(null);
+      setEditingMode(false);
+      setEditingCommentPk(null);
+      setCommentContent("");
       await fetchComments(currentPage);
     } catch (error) {
       console.error("댓글 수정 중 오류 발생:", error);
@@ -150,6 +162,7 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
       });
     }
   };
+
   const handleDelete = async (commentPk: number) => {
     showConfirm({
       message: "댓글을 삭제할까요?",
@@ -174,12 +187,12 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
     });
   };
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>, commentPk: number) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      handleEditSubmit(commentPk);
-    }
+  const handleCancelEdit = () => {
+    setEditingMode(false);
+    setEditingCommentPk(null);
+    setCommentContent("");
   };
+
   return (
     <>
       <div className="flex flex-col">
@@ -188,23 +201,10 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
           {comments.map((com) => (
             <div key={com.commentPk} className="flex justify-between pt-4">
               <div className="flex gap-2">
-                <img
-                  src={com.profileImg ?? "/img/defaultProfile.webp"}
-                  alt="프로필이미지"
-                  className="w-6 h-6 rounded-full"
-                />
+                <img src={com.profileImg ?? defaultProfileImg} alt="프로필이미지" className="w-6 h-6 rounded-full" />
                 <div className="flex flex-col gap-[2px] mb-4">
                   <p className="font-b2-semibold xl:font-b1-semibold">{com.nickname}</p>
-                  {editingComment === com.commentPk ? (
-                    <textarea
-                      value={editContent}
-                      onChange={(e) => setEditContent(e.target.value)}
-                      onKeyDown={(e) => handleKeyDown(e, com.commentPk)}
-                      className="w-[400px] xl:w-[630px] bg-grayoe-950 p-2 border font-b2-regular xl:font-b1-regular border-grayoe-600 rounded resize-none focus:outline-none"
-                    />
-                  ) : (
-                    <p className="font-b2-regular xl:font-b1-regular">{com.content}</p>
-                  )}
+                  <p className="font-b2-regular xl:font-b1-regular">{com.content}</p>
                   <p className="font-c2 text-grayoe-300">
                     {com.createTime ? formatDate(com.createTime) : "날짜 정보 없음"}
                   </p>
@@ -220,21 +220,16 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
                   />
                   {showMenu && activeCommentPk === com.commentPk && (
                     <>
-                      <div className="fixed top-0 left-0 w-[100%] h-[100%] z-10 " onClick={() => setShowMenu(false)} />
+                      <div className="fixed top-0 left-0 w-[100%] h-[100%] z-10" onClick={() => setShowMenu(false)} />
                       <div className="absolute z-20 top-6 right-0 w-14 font-c2 bg-grayoe-400 rounded-md shadow-lg flex flex-col">
-                        <button className="py-2 px-4  rounded" onClick={() => handleEdit(com.commentPk, com.content)}>
+                        <button className="py-2 px-4 rounded" onClick={() => handleEdit(com.commentPk, com.content)}>
                           수정
                         </button>
-                        <button className="py-2 px-4  rounded" onClick={() => handleDelete(com.commentPk)}>
+                        <button className="py-2 px-4 rounded" onClick={() => handleDelete(com.commentPk)}>
                           삭제
                         </button>
                       </div>
                     </>
-                  )}
-                  {editingComment === com.commentPk && (
-                    <button className="mt-2  font-bold" onClick={() => handleEditSubmit(com.commentPk)}>
-                      저장
-                    </button>
                   )}
                 </div>
               )}
@@ -242,13 +237,20 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
           ))}
         </div>
 
-        <div className="w-full px-4 py-2 bg-grayoe-400 rounded-md h-[116px]  flex flex-col justify-between gap-1">
-          <p className="mt-1 font-b2-semibold xl:font-b1-semibold">{user?.nickname}</p>
+        <div className="w-full px-4 py-2 bg-grayoe-400 rounded-md h-[116px] flex flex-col justify-between gap-1">
+          <div className="flex justify-between items-center mt-1">
+            <p className="font-b2-semibold xl:font-b1-semibold">{user?.nickname}</p>
+            {editingMode && (
+              <button onClick={handleCancelEdit} className="text-grayoe-200 font-b2-regular xl:font-b1-regular">
+                취소
+              </button>
+            )}
+          </div>
           <textarea
             value={commentContent}
             onChange={(e) => setCommentContent(e.target.value)}
             className="w-full mt-1 h-auto font-b2-regular xl:font-b1-regular focus:outline-none bg-grayoe-400 placeholder-grayoe-200 resize-none"
-            placeholder="댓글을 입력해주세요."
+            placeholder={editingMode ? "댓글을 수정해주세요." : "댓글을 입력해주세요."}
             onKeyDown={(e) => {
               if (e.key === "Enter" && !e.shiftKey) {
                 e.preventDefault();
@@ -260,9 +262,9 @@ function Comment({ communityId, setTotalComments }: CmnProps) {
             <button
               onClick={handleSendComment}
               disabled={isSending || !commentContent.trim()}
-              className={`w-11 h-7 font-b2-regular xl:font-b1-regular text-${commentContent.trim() ? "greenoe-600" : "grayoe-200"} rounded-md flex justify-center items-center `}
+              className={`w-11 h-7 font-b2-regular xl:font-b1-regular text-${commentContent.trim() ? "greenoe-600" : "grayoe-200"} rounded-md flex justify-center items-center`}
             >
-              등록
+              {editingMode ? "수정" : "등록"}
             </button>
           </div>
         </div>
